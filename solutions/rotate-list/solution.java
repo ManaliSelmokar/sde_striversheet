c74f1da129ba74d1c5832f2924f59780d27aaa8c
sudo_make_coffee <101630050+ManaliSelmokar@users.noreply.github.com>
2026-08-24 17:47:49 +0530

    Add rotate-list solution

---
+++ solutions/rotate-list/solution.java
@@ -0,0 +1,17 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode rotateRight(ListNode head, int k) {
+        if(head==null||head.next==null||k==0) return head; // edge
+
+        int len=1;
+        ListNode tail=head;
+        while(tail.next!=null){ tail=tail.next; len+
