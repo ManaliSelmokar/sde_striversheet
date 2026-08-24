3a3f2754caa0f5b0bcb56b4470dcf1e6125fb2bd
sudo_make_coffee <101630050+ManaliSelmokar@users.noreply.github.com>
2026-08-24 17:43:21 +0530

    Add merge-two-sorted-lists solution

---
+++ solutions/merge-two-sorted-lists/solution.java
@@ -0,0 +1,17 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        // Step 1: Base cases to terminate recursion 🧱
+        if (list1 == null) {
+            return list2;
+        }
+        if (list2 == null) {
