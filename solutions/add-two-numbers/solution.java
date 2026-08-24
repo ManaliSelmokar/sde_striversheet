7c8274f7f204b05af7cac35586747c926a1e4785
sudo_make_coffee <101630050+ManaliSelmokar@users.noreply.github.com>
2026-08-24 17:43:43 +0530

    Add add-two-numbers solution

---
+++ solutions/add-two-numbers/solution.java
@@ -0,0 +1,17 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(0);
+        ListNode current = dummy;
+        int carry = 0;
+
+        while (l1 != null || l2 != null || carry != 0) {
