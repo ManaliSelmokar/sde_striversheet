9ca0f71bec586dbb02a4a265e84c5b13ccb84800
sudo_make_coffee <101630050+ManaliSelmokar@users.noreply.github.com>
2026-08-24 17:39:12 +0530

    Add find-the-duplicate-number solution

---
+++ solutions/find-the-duplicate-number/solution.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int findDuplicate(int[] nums) {
+        int[] freq = new int[nums.length];
+        for(int i=0;i<nums.length;i++){
+            if(freq[nums[i]]==0){
+                freq[nums[i]]=1;
+            }
+            else{
+                return nums[i];
+            }
+        }
+        return -1;
+    }
+    
+}
